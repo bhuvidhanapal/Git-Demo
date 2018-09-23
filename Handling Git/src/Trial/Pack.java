4f9127442a7e1e4020a9d96a4ece922fb5366a4b
bhuvidhanapal <bhuvidhanapal121994@gmail.com>
2018-09-23 10:58:26 +0530

    New package Added

---
+++ Handling Git/src/Trial/Pack.java
@@ -0,0 +1,11 @@
+package Trial;
+
+public class Pack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Trial with package creation");
+	}
+
+}
